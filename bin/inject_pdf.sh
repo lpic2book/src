c8cc17f47d89f285c663f34111eadef834a4d594
Tom Scholten <git@scholten.nu>
2022-10-10 20:19:00 +0200

    Revert

--- bin/inject_pdf.sh
+++ bin/inject_pdf.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed -i 's/^plugins:/plugins:\n  - with-pdf:\n    output_path: ./site\/pdf\/lpic2book.pdf/' mkdocs.yml 
+sed -i 's#^plugins:#plugins:\n  - with-pdf:\n    output_path: .\/site\/pdf\/lpic2book.pdf#' mkdocs.yml 
